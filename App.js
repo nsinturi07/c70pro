1851c6522ef8c64ef7187a7b66999ad0cb99729e
nsinturi07 <nsinturi07@gmail.com>
2021-01-07 18:03:47 -0600

    c70pro

---
+++ App.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { createAppContainer } from "react-navigation";
+import { createBottomTabNavigator } from "react-navigation-tabs";
+
+import TransactionScreen from "./screens/WriteStory";
+
+
+export default class App extends React.Component {
+  render() {
+    return <AppContainer />;
+  }
+}
+
+const TabNavigator = createBottomTabNavigator({
+  Transaction: { screen: TransactionScreen },
+});
+
+const AppContainer = createAppContainer(TabNavigator);
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "red",
+    alignItems: "center",
+    justifyContent: "center"
+  }
+});
---
+++ screens/WriteStory.js
@@ -0,0 +1,117 @@
+
+import React from 'react';
+import { Text, View, TouchableOpacity, TextInput, Image, StyleSheet, Alert,KeyboardAvoidingView,ToastAndroid } from 'react-native';
+import * as Permissions from 'expo-permissions';
+import { BarCodeScanner } from 'expo-barcode-scanner';
+import * as firebase from "firebase";
+import db from "../config";
+
+export default class TransactionScreen extends React.Component {
+    constructor(){
+      super();
+      this.state = {
+        hasCameraPermissions: null,
+        scanned: false,
+        scannedBookId: '',
+        scannedStudentId:'',
+        buttonState: 'normal',
+        transactionMessage:''
+      }
+    }
+
+    
+    render() {
+      const hasCameraPermissions = this.state.hasCameraPermissions;
+      const scanned = this.state.scanned;
+      const buttonState = this.state.buttonState;
+
+      if (buttonState !== "normal" && hasCameraPermissions){
+        return(
+          <BarCodeScanner
+            onBarCodeScanned={scanned ? undefined : this.handleBarCodeScanned}
+            style={StyleSheet.absoluteFillObject}
+          />
+        );
+      }
+
+      else if (buttonState === "normal"){
+        return(
+          <KeyboardAvoidingView style={styles.container} behavior="padding" enabled>
+          <View> 
+              <Image
+                source={require("../assets/booklogo.jpg")}
+                style={{width:200, height: 200}}/>
+              <Text style={{textAlign: 'center', fontSize: 30}}>Wily</Text>
+            </View>
+            <View style={styles.inputView}>
+            <TextInput 
+              style={styles.inputBox}
+              placeholder="type author name"
+              onChangeText={text=>{
+                this.setState({scannedBookId:text})
+              }}
+              value={this.state.scannedBookId}/>
+            
+            </View>
+            <View style={styles.inputView}>
+            <TextInput 
+              style={styles.inputBox}
+              placeholder="Type story name"
+              onChangeText={text=>{
+                this.setState({scannedStudentId:text})
+              }}
+              value={this.state.scannedStudentId}/>
+            
+            
+            </View>
+            <TouchableOpacity style={styles.submitButton} onPress={async()=>{this.handleTransaction()}}>
+            <Text style={styles.displayText}>
+            submit
+            </Text>
+            </TouchableOpacity>
+          </KeyboardAvoidingView>
+        );
+      }
+    }
+  }
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center'
+    },
+    displayText:{
+      fontSize: 15,
+      textDecorationLine: 'underline'
+    },
+    scanButton:{
+      backgroundColor: '#2196F3',
+      padding: 10,
+      margin: 10
+    },
+    buttonText:{
+      fontSize: 15,
+      textAlign: 'center',
+      marginTop: 10
+    },
+    inputView:{
+      flexDirection: 'row',
+      margin: 20
+    },
+    inputBox:{
+      width: 200,
+      height: 40,
+      borderWidth: 1.5,
+      borderRightWidth: 0,
+      fontSize: 20
+    },
+    submitButton:{
+      backgroundColor: '#66BB6A',
+      width: 50,
+      borderWidth: 1.5,
+      borderLeftWidth: 0
+    }
+  });
+
+
+  
