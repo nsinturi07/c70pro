86503543072097ca4c845e0496102121d5e687e1
nsinturi07 <65034759+nsinturi07@users.noreply.github.com>
2021-01-24 17:53:18 -0600

    Add files via upload

---
+++ config.js
@@ -0,0 +1,14 @@
+import firebase from 'firebase'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAvcUKWu3ZeypLMpWV8Gj9DfRCZCdnu2Ec",
+    authDomain: "story-hub-pro.firebaseapp.com",
+    databaseURL: "https://story-hub-pro-default-rtdb.firebaseio.com",
+    projectId: "story-hub-pro",
+    storageBucket: "story-hub-pro.appspot.com",
+    messagingSenderId: "104942771739",
+    appId: "1:104942771739:web:fb6a036d028ad1ba8ca2be"
+  };
+
+  export default firebase.firebase();
+  
